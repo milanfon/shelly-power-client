c50a829fbf1ca7918ccaff0e0fbb15e739def92b
MML Benchmark <benchmark@milanfon.cz>
2023-06-24 07:39:42 +0200

    FC

---
+++ app.js
@@ -0,0 +1,84 @@
+const mqtt = require('mqtt');
+const readLine = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+const fs = require('fs');
+
+const conf = {
+    host: '10.0.5.20',
+    port: '1883',
+    clientId: 'MML_PC',
+    topic: 'shellypro4pm-30c6f7840474/events/rpc'
+}
+
+let sum = 0;
+let lastTimestamp = 0;
+let lastPower = 0;
+let startTime = 0;
+
+const urlString = `mqtt://${conf.host}:${conf.port}`;
+
+function createFiles(dir){
+    fs.writeFileSync(dir+'/log.csv', ["id", "ts", "apower"].map(i => i+';').join('')+'\r\n');
+    fs.writeFileSync(dir+'/summary.json', "");
+}
+
+function addToLog(dir, o){
+    fs.appendFileSync(dir+'/log.csv', [o.src, o.params.ts, o.params["switch:0"].apower].map(i => i+';').join('')+'\r\n');
+}
+
+function writeSummary(dir){
+    const elapsedSecs = parseInt(((new Date())-startTime)/1000);
+    fs.writeFileSync(dir+'/summary.json', JSON.stringify({
+        totalPower: sum,
+        elapsedTime: elapsedSecs,
+        avgHourlyPower: (sum/elapsedSecs)*3600
+    }));
+}
+
+readLine.question('Enter filename: ', (filename) => {
+    readLine.write(`Creating test '${filename}'\n`);
+    const dir = 'out/'+filename;
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir);
+        createFiles(dir);
+    } else {
+        readLine.write('File already exists! Exiting...');
+        process.exit();
+    }
+
+    const client = mqtt.connect(urlString, {clientId: conf.clientId});
+    startTime = new Date();
+
+    client.on('connect', () => {
+        client.subscribe([conf.topic]);
+    });
+
+    client.on('message', (topic, payload) => {
+        const o = JSON.parse(payload.toString());
+        try {
+            if (o?.params['switch:0'].hasOwnProperty('apower')){
+                sum += (o.params.ts-lastTimestamp)*lastPower/3600;
+                lastTimestamp = o.params.ts;
+                lastPower = o.params['switch:0'].apower;
+                addToLog(dir, o);
+                console.log(sum);
+            }
+        } catch (e){
+            console.log('Corupted packet received');
+        }
+    });
+
+    process.stdin.on('keypress', (str, key) => {
+        if (key.ctrl && key.name === 'x'){
+            console.log('Saving results and ending...');
+            client.removeAllListeners();
+            writeSummary(dir);
+            process.exit();
+        }
+    });
+});
+
+
+
